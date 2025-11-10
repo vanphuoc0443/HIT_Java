570faa21dbfb60f73c61a6ebb69010c94c2dd4f3
vanphuoc0443 <vanphuoc0443@gmail.com>
2025-11-10 22:42:08 +0700

    feat: add BTVNBuoi4

---
+++ BTVNBuoi4/src/productapp/common/Constants.java
@@ -0,0 +1,9 @@
+package productapp.common;
+
+public class Constants {
+    public static final String EMPTY_NAME = "Tên sản phẩm không được để trống!";
+    public static final String NEGATIVE_PRICE = "Giá sản phẩm không được âm!";
+    public static final String INVALID_TYPE = "Loại sản phẩm không hợp lệ!";
+    public static final String ADD_SUCCESS = "Thêm sản phẩm thành công!";
+    public static final String ADD_FAILED = "Không thể thêm sản phẩm!";
+}
